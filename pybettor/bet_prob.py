6a9c82f39ce1b4c73a31800e9ae1f2cb12eb0363
Carl Zhou <dev@carlzhou.com>
2023-05-26 12:42:00 -0400

    refactor plot results

--- pybettor/bet_prob.py
+++ pybettor/bet_prob.py
@@ -3,6 +3,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def _plot_results(probabilities, pred_spread, spread, sd):
+    win_prob = probabilities['win_prob']
+    push_prob = probabilities['push_prob']
+    lose_prob = probabilities['lose_prob']
+
+    fig, ax = plt.subplots()
+
+    x = np.linspace(pred_spread - (sd * 3), pred_spread + (sd * 3), 501)
+    ax.plot(x, stats.norm.pdf(x, loc=pred_spread, scale=sd),
+            "b-", label="Normal Distribution")
+
+    x_left = np.linspace(pred_spread - (sd * 3), spread, 501)
+    ax.fill_between(x_left, stats.norm.pdf(
+        x_left, loc=pred_spread, scale=sd), color="limegreen", alpha=0.75)
+
+    x_right = np.linspace(spread, pred_spread + (sd * 3), 501)
+    ax.fill_between(x_right, stats.norm.pdf(
+        x_right, loc=pred_spread, scale=sd), color="salmon", alpha=0.75)
+
+    ax.axvline(spread, color="black", linestyle="--", label="Spread")
+    ax.axvline(pred_spread, color="black", linestyle="--", label="Prediction")
+
+    annotation_text = (
+        f"Point Edge: {spread - pred_spread}\n"
+        f"Win Probability: {round(win_prob * 100, 2)}%\n"
+        f"Lose Probability: {round(lose_prob * 100, 2)}%\n"
+        f"Push Probability: {round(push_prob * 100, 2)}%"
+    )
+    ax.annotate(
+        annotation_text,
+        xy=(pred_spread - (sd * 3), ax.get_ylim()[1]),
+        xytext=(-15, -2),
+        textcoords="offset points",
+        ha="left",
+        va="top",
+        fontsize=10,
+        fontweight="bold",
+    )
+
+    ax.set_xlabel("Final Margin")
+    ax.set_ylabel("")
+    ax.set_title(f"Predicted Line: {pred_spread} - Actual Line: {spread}")
+
+    ax.set_ylim(0, None)
+    ax.yaxis.set_ticks([])
+    ax.set_facecolor("white")
+
+    return fig
+
+
 def bet_prob(pred_spread: float, spread: float, sport: str = "NBA", plot=False) -> dict or tuple:
     """
     Calculates the probability of winning, losing, and pushing a bet based on the predicted spread and actual spread.
@@ -29,53 +79,14 @@ def bet_prob(pred_spread: float, spread: float, sport: str = "NBA", plot=False) 
     lose_prob = stats.norm.cdf(pred_spread - 0.5, loc=spread, scale=sd)
     push_prob = 1 - win_prob - lose_prob if spread % 1 == 0 else 0
 
-    probabilities = {"win_prob": win_prob,
-                     "lose_prob": lose_prob, "push_prob": push_prob}
+    probabilities = {
+        "win_prob": win_prob,
+        "lose_prob": lose_prob,
+        "push_prob": push_prob,
+    }
 
     if plot:
-        fig, ax = plt.subplots()
-
-        x = np.linspace(pred_spread - (sd * 3), pred_spread + (sd * 3), 501)
-        ax.plot(x, stats.norm.pdf(x, loc=pred_spread, scale=sd),
-                "b-", label="Normal Distribution")
-
-        x_left = np.linspace(pred_spread - (sd * 3), spread, 501)
-        ax.fill_between(x_left, stats.norm.pdf(
-            x_left, loc=pred_spread, scale=sd), color="limegreen", alpha=0.75)
-
-        x_right = np.linspace(spread, pred_spread + (sd * 3), 501)
-        ax.fill_between(x_right, stats.norm.pdf(
-            x_right, loc=pred_spread, scale=sd), color="salmon", alpha=0.75)
-
-        ax.axvline(spread, color="black", linestyle="--", label="Spread")
-        ax.axvline(pred_spread, color="black",
-                   linestyle="--", label="Prediction")
-
-        annotation_text = (
-            f"Point Edge: {spread - pred_spread}\n"
-            f"Win Probability: {round(win_prob * 100, 2)}%\n"
-            f"Lose Probability: {round(lose_prob * 100, 2)}%\n"
-            f"Push Probability: {round(push_prob * 100, 2)}%"
-        )
-        ax.annotate(
-            annotation_text,
-            xy=(pred_spread - (sd * 3), ax.get_ylim()[1]),
-            xytext=(-15, -2),
-            textcoords="offset points",
-            ha="left",
-            va="top",
-            fontsize=10,
-            fontweight="bold",
-        )
-
-        ax.set_xlabel("Final Margin")
-        ax.set_ylabel("")
-        ax.set_title(f"Predicted Line: {pred_spread} - Actual Line: {spread}")
-
-        ax.set_ylim(0, None)
-        ax.yaxis.set_ticks([])
-        ax.set_facecolor("white")
-
+        fig = _plot_results(probabilities, pred_spread, spread, sd)
         return probabilities, fig
 
     return probabilities
