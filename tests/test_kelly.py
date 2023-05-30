85e9cd859f5148952ad91491fb9408be288a10ed
Carl Zhou <dev@carlzhou.com>
2023-05-30 11:24:28 -0400

    fix kelly test

--- tests/test_kelly.py
+++ tests/test_kelly.py
@@ -7,12 +7,12 @@ class TestKelly(unittest.TestCase):
     def test_kelly(self):
         win_prob = 0.6
         odds = 100
-        npt.assert_almost_equal(kelly(win_prob, odds), 0.2)
+        npt.assert_almost_equal(kelly(win_prob, odds), 20)
 
         win_prob = 0.6
         odds = 2.0
         category = "dec"
-        npt.assert_almost_equal(kelly(win_prob, odds, category), 0.2)
+        npt.assert_almost_equal(kelly(win_prob, odds, category), 20)
 
 
 if __name__ == "__main__":
